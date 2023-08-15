bdf7fe8f7706c1dae82205add9e8a2f45b620488
NAVEEN APPANA <appananaveen19@gmail.com>
2023-08-15 23:58:00 +0530

    but

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,14 +1,12 @@
-<section class="">
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img src="./best.jpg" alt="image" class="w-full p-4">
-    
-    <!-- <div class="image m-6 hidden flex-initial md:block min-w-[50%]" /> -->
-</section>
+<section
+    class="flex flex-col h-[100vh] aspect-auto justify-center items-center"
+>
+    <img src="./best.jpg" alt="hllo" class="w-full p-4 md:w-[80vw]" />
 
-<style>
-    /* .image {
-		background: linear-gradient(180deg, rgba(27, 19, 24, 0.3), rgba(12, 4, 8, 0.425)),
-			url('../images/best.jpg');
-		background-size: cover;
-	} */
-</style>
+    <a href="https://api.whatsapp.com/send?phone=919502876666">
+        <button
+            class="text-black text-sm md:text-xl md:mt-[-30px] hover:bg-slate-400 hover:text-black border-spacing-6 border-2 bg-cyan-400"
+            >Start Now</button
+        >
+    </a>
+</section>
